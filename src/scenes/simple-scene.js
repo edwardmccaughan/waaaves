ac7d154567c7e5b1d7d815dc373c7dffbdfe1eb6
edward mccaughan <edwardmccaughan@gmail.com>
2019-01-19 17:00:19 +0100

    smooth decay

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -3,12 +3,15 @@ import { RealKeyboard } from '../real_keyboard'
 import { note_frequencies } from '../note_frequencies'
 
 export class SimpleScene extends Phaser.Scene {
-  preload() {
-    console.log(note_frequencies)
-  }
+  preload() {}
 
   create() {
-    this.keys_down = []
+    this.decay_rate = 5
+    this.x_scaling = 1200
+    this.y_scaling = 100
+    this.line_color = 0x00ffff
+
+    this.prefill_note_levels()
     this.graphics = this.add.graphics()
 
     new MidiController( (key) => { this.key_down(key) }, (key) => { this.key_up(key)} )
@@ -16,36 +19,64 @@ export class SimpleScene extends Phaser.Scene {
   }
 
   update() {
-    this.line_test()
+    this.decay_note_levels()
+    this.draw_line()
   }
 
-  line_test() {
-    var path = new Phaser.Curves.Path(0, 100);
-    for(var x = 0; x < window.innerWidth; x++) {
-      var real_sin = this.keys_down.reduce((memo, midi_key) => memo + this.note_amplitude(midi_key, x), 0);
-      var y =  80 * real_sin + (window.innerHeight / 2)
-      //console.log(n, y)
-      path.splineTo([x, y]) ;
-    }
+  decay_note_levels(){
+    Object.keys(this.note_levels).forEach((key) => { 
+      if(this.note_levels[key] > 0 && !this.notes_pressed[key]){
+        this.note_levels[key] -= this.decay_rate
+      }
+    })
+  }
 
+  prefill_note_levels() {
+    this.note_levels = {}
+    this.notes_pressed = {}
+    for(var x = 0; x < note_frequencies.length; x++) {
+      this.note_levels[x] = 0
+      this.notes_pressed[x] = false
+    }
+  } 
+
+
+  calculate_line() {
+    this.path = new Phaser.Curves.Path(0, window.innerHeight / 2);
+    for(var x = 0; x < window.innerWidth; x++) {
+      
+      var real_sin = Object.keys(this.note_levels).reduce((memo, midi_key) => { 
+       return  memo + this.note_amplitude(midi_key, x)
+      }, 0)
+
+      var y = real_sin + (window.innerHeight / 2)
+
+      this.path.splineTo([x, y]) ;
+    }
+  }
+
+  draw_line() {
+    this.calculate_line()
     this.graphics.clear()
 
-    this.graphics.lineStyle(2, 0x00ffff, 1);
-    path.draw(this.graphics);
+    this.graphics.lineStyle(2, this.line_color, 1);
+    this.path.draw(this.graphics);
   }
 
   note_amplitude(midi_key, x) {
-    return  Math.sin(note_frequencies[midi_key] * x / 600)
+    var raw_amplitude =  Math.sin(note_frequencies[midi_key] * x / this.x_scaling)
+    var scaled_amplitude = raw_amplitude * this.note_levels[midi_key]
+    return scaled_amplitude
   }
 
 
   key_down(key) {
-    this.keys_down.push(key) 
+    this.notes_pressed[key] = true
+    this.note_levels[key] = this.y_scaling
   }
 
   key_up(key) {
-    // remove from the array, there's probably a more efficient way to do this
-    this.keys_down = this.keys_down.filter(item => item !== key )
+    this.notes_pressed[key] = false
   }
 
 }
